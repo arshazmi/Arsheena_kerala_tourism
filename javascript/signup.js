2783c50a47b01687f50d419a40fb9f6a89855337
arsheena_azmi <aazmi600@gmail.com>
2020-11-16 14:29:38 +0530

    first version

---
+++ javascript/signup.js
@@ -0,0 +1,95 @@
+/*email validation*/
+
+let email=document.getElementById("email");
+function validateml(){
+  let regexp=/^([A-Za-z0-9\.\-]+)@([A-Za-z0-9\-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/
+ if((regexp.test(email.value))&&email.value.trim()!==""){
+    return true;
+  }
+  else{
+    alert("Invalid Email Format.");
+    return false;
+  }
+ }
+
+/*password strength checker*/
+
+const indicator = document.querySelector(".indicator");
+const input = document.querySelector(".pass");
+const weak = document.querySelector(".weak");
+const medium = document.querySelector(".medium");
+const strong = document.querySelector(".strong");
+const text = document.querySelector(".text");
+const showBtn = document.querySelector(".showBtn");
+  let regExpWeak = /[a-z]/;
+  let regExpMedium = /\d+/;
+  let regExpStrong = /.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/;
+    function trigger(){
+        if(input.value != ""){
+          indicator.style.display = "block";
+          indicator.style.display = "flex";
+          if(input.value.length <= 3 && (input.value.match(regExpWeak) || input.value.match(regExpMedium) || input.value.match(regExpStrong)))no=1;
+          if(input.value.length >= 6 && ((input.value.match(regExpWeak) && input.value.match(regExpMedium)) || (input.value.match(regExpMedium) && input.value.match(regExpStrong)) || (input.value.match(regExpWeak) && input.value.match(regExpStrong))))no=2;
+          if(input.value.length >= 6 && input.value.match(regExpWeak) && input.value.match(regExpMedium) && input.value.match(regExpStrong))no=3;
+          if(no==1){
+            weak.classList.add("active");
+            text.style.display = "block";
+            text.textContent = "Your password is too week";
+            text.classList.add("weak");
+          }
+          if(no==2){
+            medium.classList.add("active");
+            text.textContent = "Your password is medium";
+            text.classList.add("medium");
+          }else{
+            medium.classList.remove("active");
+            text.classList.remove("medium");
+          }
+          if(no==3){
+            weak.classList.add("active");
+            medium.classList.add("active");
+            strong.classList.add("active");
+            text.textContent = "Your password is strong";
+            text.classList.add("strong");
+          }else{
+            strong.classList.remove("active");
+            text.classList.remove("strong");
+          }
+          showBtn.style.display = "block";
+          showBtn.onclick = function(){
+            if(input.type == "password"){
+              input.type = "text";
+              showBtn.textContent = "HIDE";
+              showBtn.style.color = "#23ad5c";
+            }else{
+              input.type = "password";
+              showBtn.textContent = "SHOW";
+              showBtn.style.color = "#000";
+            }
+          }
+        }else{
+          indicator.style.display = "none";
+          text.style.display = "none";
+          showBtn.style.display = "none";
+        }
+      }
+
+/*phone validation*/
+
+let phone=document.getElementById("phone");
+function validatephn(){
+    let regex=/^\(?([0-9]{3})\)?[- .]?([0-9]{3})[- .]?([0-9]{4})$/ 
+    if(regex.test(phone.value)){
+      return true;
+    }
+    else{
+    alert("Invalid Phone Number Format.It should contain 10 numbers only. Additionally accept only 3 formats: XXX.XXX.XXXX, XXX-XXX-XXXX, XXX XXX XXXX ");
+      return false;
+    }
+  }
+
+  /* function validation*/
+  function validate(){
+    return(validateml()&&validatephn());
+  }
+  
